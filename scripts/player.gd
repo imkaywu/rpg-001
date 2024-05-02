0839f7676323d16a1c8c3ab31cd2cc4dca58dbee
imkaywu <imkaywu@gmail.com>
2024-05-01 23:23:52 -0700

    make player animation right

--- scripts/player.gd
+++ scripts/player.gd
@@ -4,6 +4,11 @@ const Acceleration = 500
 const MaxSpeed = 80
 const Friction = 500
 
+@onready var animation_player = $AnimationPlayer
+@onready var animation_tree = $AnimationTree
+@onready var animation_state = animation_tree.get("parameters/playback")
+
+
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _physics_process(delta):
 	var dir = Vector2.ZERO
@@ -12,8 +17,12 @@ func _physics_process(delta):
 	dir = dir.normalized()
 	
 	if dir != Vector2.ZERO:
+		animation_tree.set("parameters/Idle/blend_position", dir)
+		animation_tree.set("parameters/Run/blend_position", dir)
+		animation_state.travel("Run")
 		velocity = velocity.move_toward(dir * MaxSpeed, Acceleration * delta)
 	else:
+		animation_state.travel("Idle")
 		velocity = velocity.move_toward(Vector2.ZERO, Friction * delta)
 	
 	move_and_slide()
